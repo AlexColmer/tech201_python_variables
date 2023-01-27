ff2e0afa6b6da9ee61dbf36b4c84f30a5951193f
Alex Colmer <alexander.colmer@gmail.com>
2023-01-27 15:55:16 +0000

    added name age and DOB print out

---
+++ variables.py
@@ -0,0 +1,34 @@
+# what is a variable it is a way to store different data types. flexible to change.
+
+# variables
+# varible_name = variable_data
+
+a = 1
+b = 2
+c = 3.5
+hi = "Hello World!"
+
+# 'controll + /' will comment out multiple lines
+# print(hi)
+# print(a)
+
+# print(1 + 2)
+# print (a + b)
+
+# the type method
+
+# print(type(c))
+
+# overwriting variables
+
+# print(a)
+# a = 4
+# print(a)
+
+# getting user input
+print("Hi, what is your name?")
+name = input()
+age = input()
+DOB =input()
+print("My name is " + name + " age is, " + age + " date of birth is " + DOB)
+
